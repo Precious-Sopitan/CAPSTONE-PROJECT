ebd6e6325902f0af13e5c0c50f85540763818a74
Sopitan Precious Abolade <125654024+Precious-Sopitan@users.noreply.github.com>
2024-04-16 13:28:49 +0100

    update

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import SigninPage from './Components/SigninPage'
+
+function App() {
+
+
+  return (
+    <>
+     <SigninPage/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/SigninPage.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import Logo from "../assets/Logo.png"
+
+
+const SigninPage = () => {
+  return (
+    <div class='container' >
+
+    <div class="side1">
+      
+    </div>
+
+    <div class="side2">
+        <div id='form'>
+            <p class='logo'>
+                <img src={Logo} alt="" srcset="" />
+            </p>
+            <span class='welcome'> WELCOME BACK </span>
+   
+        </div>
+    </div>
+   
+  </div>
+  )
+}
+
+export default SigninPage
